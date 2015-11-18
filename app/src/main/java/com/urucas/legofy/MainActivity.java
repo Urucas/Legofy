a3b79f28774f20bf2a6daec85abb0da0818809cd
vrunoa <vruno.alassia@gmail.com>
2015-11-18 11:04:41 -0800

    drawing lego images, not the perfect script. Should set the color filter to the lego bitmap.

--- app/src/main/java/com/urucas/legofy/MainActivity.java
+++ app/src/main/java/com/urucas/legofy/MainActivity.java
@@ -30,43 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // getSupportActionBar().hide();
-
         setContentView(new LegoView(MainActivity.this));
-
-        /*
-        WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
-        Display display = wm.getDefaultDisplay();
-        int height, width;
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            Point size = new Point();
-            display.getSize(size);
-            height = size.y;
-            width = size.x;
-        }else {
-            height = display.getHeight();
-            width = display.getWidth();
-        }
-
-        LinearLayout ll = (LinearLayout) findViewById(R.id.canvas);
-        Bitmap bg = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-
-        Paint paint = new Paint();
-        Canvas canvas = new Canvas(bg);
-
-        Drawable d = getResources().getDrawable(R.drawable.brick);
-        Bitmap brick = BitmapFactory.decodeResource(getResources(), R.drawable.brick);
-        canvas.drawBitmap(brick, 0, 0, null);
-        */
-
-    }
-
-    public static float convertPixelsToDp(float px, Context context){
-        Resources resources = context.getResources();
-        DisplayMetrics metrics = resources.getDisplayMetrics();
-        float dp = px / (metrics.densityDpi / 160f);
-        return dp;
     }
 
     public class LegoView extends SurfaceView implements SurfaceHolder.Callback2 {
@@ -88,9 +52,6 @@ public class MainActivity extends AppCompatActivity {
                 // set canvas background color to white
                 canvas.drawColor(Color.WHITE);
                 int cw = canvas.getWidth(), ch = canvas.getHeight();
-                Log.i("canvas width", String.valueOf(cw));
-                Log.i("canvas height", String.valueOf(ch));
-
 
                 // resize flower image to fit screen width
                 Bitmap flower = BitmapFactory.decodeResource(getResources(), R.drawable.flower);
@@ -111,9 +72,6 @@ public class MainActivity extends AppCompatActivity {
                     flower.recycle();
                 }
 
-                Log.i("flower resized width", String.valueOf(flower_width));
-                Log.i("flower resized height", String.valueOf(flower_height));
-
                 // resize brick
                 Bitmap brick = BitmapFactory.decodeResource(getResources(), R.drawable.brick);
                 int brick_width = (int)(brick.getWidth()*0.4f), brick_height = (int)(brick.getHeight()*0.4f);
@@ -123,34 +81,39 @@ public class MainActivity extends AppCompatActivity {
                 while(y < flower_height) {
                     while(x < flower_width) {
 
+                        // get image pixel center colour
                         int pos_x = x + brick_width/2, pos_y = y + brick_height/2;
-                        String pos = String.valueOf(pos_x)+","+String.valueOf(pos_y);
+                        int colour = flowerResized.getPixel(pos_x, pos_y);
+                        int r = Color.red(colour),
+                            g = Color.green(colour),
+                            b = Color.blue(colour);
 
-                        ColorMatrix colorMatrix = new ColorMatrix();
-                        // colorMatrix.setSaturation(0f);
-                        int colour = flowerResized.getPixel(pos_x,pos_y);
-
-                        float[] colorTransform = {
-                                0, 0, 0, 0, Color.red(colour),
-                                0, 0, 0, 0, Color.green(colour),
-                                0, 0, 0, 0, Color.blue(colour),
-                                0, 0, 0, 1f, 0
-                        };
-
-                        colorMatrix.set(colorTransform);
-
-                        ColorMatrixColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
+                        // draw square
                         Paint paint = new Paint();
-                        paint.setColorFilter(colorFilter);
+                        paint.setColor(Color.rgb(r, g, b));
+                        canvas.drawRect(x, y, x + brick_width, y + brick_height, paint);
 
-                        canvas.drawBitmap(brickResized, x, y, paint);
+                        // draw brick
+                        ColorMatrix colorMatrix = new ColorMatrix();
+                        colorMatrix.setSaturation(0f);
+                        float[] colorTransform = {
+                                0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0,
+                                0, 0, 0, 0.3f, 0
+                        };
+                        colorMatrix.set(colorTransform);
+                        ColorMatrixColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
+                        Paint paint1 = new Paint();
+                        paint1.setColorFilter(colorFilter);
+                        canvas.drawBitmap(brickResized, x, y, paint1);
 
                         x+= brick_width;
                     }
                     x = 0;
                     y+= brick_height;
                 }
-                //canvas.drawBitmap(flower, 0, 0, null);
+                // canvas.drawBitmap(flowerResized, 0, 0, null);
                 surfaceHolder.unlockCanvasAndPost(canvas);
             }
         }
