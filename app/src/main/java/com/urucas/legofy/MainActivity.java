8f7844883ae4adc6e43353489607ff722c0cc2b2
vrunoa <vruno.alassia@gmail.com>
2015-11-27 10:06:51 -0800

    add crashlytics

--- app/src/main/java/com/urucas/legofy/MainActivity.java
+++ app/src/main/java/com/urucas/legofy/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         Button cameraButton = (Button) findViewById(R.id.cameraBtt);
