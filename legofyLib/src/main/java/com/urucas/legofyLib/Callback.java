8321abc78e14cedad6091dafa3d8eea452bfc8ee
vrunoa <vruno.alassia@gmail.com>
2015-12-10 20:59:20 -0800

    fix x > bitmap.getWidth() bug, add interface callback

---
+++ legofyLib/src/main/java/com/urucas/legofyLib/Callback.java
@@ -0,0 +1,9 @@
+package com.urucas.legofyLib;
+
+/**
+ * Created by vruno on 12/10/15.
+ */
+public interface Callback {
+
+    void done(Exception e);
+}
--- legofyLib/src/main/java/com/urucas/legofyLib/Legofy.java
+++ legofyLib/src/main/java/com/urucas/legofyLib/Legofy.java
@@ -19,81 +19,96 @@ import com.urucas.legofyLib.R;
 
 public abstract class Legofy {
 
-    public static void me(Context context, Canvas canvas, int imageResource) {
+    public static void me(Context context, Canvas canvas, int imageResource, Callback callback) {
         Resources res = context.getResources();
         Bitmap bmp = BitmapFactory.decodeResource(res, imageResource);
-        Legofy.me(context, canvas, bmp);
+        Legofy.me(context, canvas, bmp, callback);
     }
 
-    public static void me(Context context, Canvas canvas, Bitmap bmp) {
+    public static void me(Context context, Canvas canvas, Bitmap bmp, Callback callback) {
 
-        // set canvas background color to white
-        canvas.drawColor(Color.BLACK);
-        int cw = canvas.getWidth(), ch = canvas.getHeight();
+            // set canvas background color to white
+            canvas.drawColor(Color.BLACK);
+            int cw = canvas.getWidth(), ch = canvas.getHeight();
 
-        // resize flower image to fit screen width
-        Bitmap bmpResized;
-        int bmpWidth = bmp.getWidth(), bmpHeight = bmp.getHeight();
+            // resize flower image to fit screen width
+            Bitmap bmpResized;
+            int bmpWidth = bmp.getWidth(), bmpHeight = bmp.getHeight();
 
-        if(bmpWidth < cw) {
-            float percent = (cw*100)/bmpWidth;
-            bmpWidth = cw;
-            float scaleHeight = (percent*bmpHeight)/100;
-            bmpResized = Bitmap.createScaledBitmap(bmp, bmpWidth, (int)scaleHeight, false);
-            bmpHeight = scaleHeight > ch ? ch : (int) scaleHeight;
-            // bmp.recycle();
-        }else{
-            float percent = (cw*100)/bmpWidth;
-            float scaleHeight = bmpHeight*(percent/100);
-            bmpResized = Bitmap.createScaledBitmap(bmp, cw, (int)scaleHeight, false);
-            bmpWidth = cw;
-            bmpHeight = (int)scaleHeight;
-            // bmp.recycle();
-        }
-
-        // resize brick
-        Resources res = context.getResources();
-        Bitmap brick = BitmapFactory.decodeResource(res, R.drawable.brick);
-        int brick_width = (int)(brick.getWidth()*0.4f), brick_height = (int)(brick.getHeight()*0.4f);
-        Bitmap brickResized = Bitmap.createScaledBitmap(brick, brick_width, brick_height, false);
-
-        int y = 0, x = 0;
-        while(y < bmpHeight) {
-            while(x < bmpWidth) {
-                // get image pixel center colour
-                int pos_x = x + brick_width/2, pos_y = y + brick_height/2;
-                int colour = bmpResized.getPixel(pos_x, pos_y);
-                int r = Color.red(colour),
-                        g = Color.green(colour),
-                        b = Color.blue(colour);
-
-                // draw square
-                Paint paint = new Paint();
-                paint.setColor(Color.rgb(r, g, b));
-                canvas.drawRect(x, y, x + brick_width, y + brick_height, paint);
-
-                // draw brick
-                ColorMatrix colorMatrix = new ColorMatrix();
-                colorMatrix.setSaturation(0f);
-                float[] colorTransform = {
-                        0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0,
-                        0, 0, 0, 0.3f, 0
-                };
-                colorMatrix.set(colorTransform);
-                ColorMatrixColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
-
-                Paint paint1 = new Paint();
-                paint1.setColorFilter(colorFilter);
-                canvas.drawBitmap(brickResized, x, y, paint1);
-
-                x+= brick_width;
+            if (bmpWidth < cw) {
+                float percent = (cw * 100) / bmpWidth;
+                bmpWidth = cw;
+                float scaleHeight = (percent * bmpHeight) / 100;
+                bmpResized = Bitmap.createScaledBitmap(bmp, bmpWidth, (int) scaleHeight, false);
+                bmpHeight = bmpResized.getHeight();
+                bmpWidth = bmpResized.getWidth();
+                // bmp.recycle();
+            } else {
+                float percent = (cw * 100) / bmpWidth;
+                float scaleHeight = bmpHeight * (percent / 100);
+                bmpResized = Bitmap.createScaledBitmap(bmp, cw, (int) scaleHeight, false);
+                bmpWidth = bmpResized.getWidth();
+                bmpHeight = bmpResized.getHeight();
+                // bmp.recycle();
             }
-            x = 0;
-            y+= brick_height;
 
+            // resize brick
+            Resources res = context.getResources();
+            Bitmap brick = BitmapFactory.decodeResource(res, R.drawable.brick);
+            int brick_width = (int) (brick.getWidth() * 0.4f), brick_height = (int) (brick.getHeight() * 0.4f);
+            Bitmap brickResized = Bitmap.createScaledBitmap(brick, brick_width, brick_height, false);
+
+            int y = 0, x = 0;
+            int pos_x = 0, pos_y = 0;
+            try {
+
+            while (y < bmpHeight) {
+                while (x < bmpWidth) {
+                    // get image pixel center colour
+                    pos_x = x + brick_width / 2;
+                    pos_y = y + brick_height / 2;
+                    if(pos_x >= bmpWidth) {
+                        pos_x = bmpWidth-1;
+                    }
+                    if(pos_y >= bmpHeight) {
+                        pos_y = bmpHeight-1;
+                    }
+                    int colour = bmpResized.getPixel(pos_x, pos_y);
+                    int r = Color.red(colour),
+                            g = Color.green(colour),
+                            b = Color.blue(colour);
+
+                    // draw square
+                    Paint paint = new Paint();
+                    paint.setColor(Color.rgb(r, g, b));
+                    canvas.drawRect(x, y, x + brick_width, y + brick_height, paint);
+
+                    // draw brick
+                    ColorMatrix colorMatrix = new ColorMatrix();
+                    colorMatrix.setSaturation(0f);
+                    float[] colorTransform = {
+                            0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0,
+                            0, 0, 0, 0.3f, 0
+                    };
+                    colorMatrix.set(colorTransform);
+                    ColorMatrixColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
+
+                    Paint paint1 = new Paint();
+                    paint1.setColorFilter(colorFilter);
+                    canvas.drawBitmap(brickResized, x, y, paint1);
+
+                    x += brick_width;
+                }
+                x = 0;
+                y += brick_height;
+            }
+            callback.done(null);
+
+            // canvas.drawBitmap(bmpResized, 0, 0, new Paint());
+        }catch(Exception e){
+            callback.done(e);
         }
-        // canvas.drawBitmap(bmpResized, 0, 0, new Paint());
     }
 }
